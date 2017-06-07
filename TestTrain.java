f0625ad2233035cde7d58487bea39d0d3a38c81b
Bruno Mermet <Bruno.Mermet@univ-lehavre.fr>
2017-06-08 01:42:21 +0200

    Gestion des trains avec composition incorrecte au moyen d'exceptions

--- TestTrain.java
+++ TestTrain.java
@@ -8,7 +8,7 @@ import train.vehicule.Wagon;
 public class TestTrain
 {
 
-	public static void main(String[] arg)
+	public static void main(String[] arg) throws Exception
 	{
 		Train t1 = new Train();
 
@@ -16,13 +16,35 @@ public class TestTrain
 		t1.ajouterVehicule ( new Voiture ( 40.3,  8,  65) );
 		t1.ajouterVehicule ( new Voiture ( 40.3,  8,  80) );
 		t1.ajouterVehicule ( new Wagon   ( 25.5,  8, 250) );
-		t1.ajouterVehicule ( new Motrice (  110, 12,   5) );   // ajout impossible
+		try {
+		    // ajout impossible
+		    t1.ajouterVehicule ( new Motrice (  110, 12,   5) );
+		    throw new Exception("On ne devrait pas arriver là");
+		}
+		catch(RuntimeException re) {
+		    System.err.println("ajout 1 impossible : normal");
+		}
+
 
 		System.out.println ( t1 );
 
 		Train t2 = new Train();
-		t2.ajouterVehicule ( new Voiture ( 40.3,  8,  65) );  // ajout impossible
-		t2.ajouterVehicule ( new Wagon   ( 25.5,  8, 250) );  // ajout impossible
+		try {
+		    // ajout impossible
+		    t2.ajouterVehicule ( new Voiture ( 40.3,  8,  65) );
+		    throw new Exception("On ne devrait pas arriver là");
+		}
+		catch(RuntimeException re) {
+		    System.err.println("ajout 2 impossible : normal");
+		}
+		try {
+		    // ajout impossible
+		    t2.ajouterVehicule ( new Wagon   ( 25.5,  8, 250) );
+		    throw new Exception("On ne devrait pas arriver là");
+		}
+		catch(RuntimeException re) {
+		    System.err.println("ajout 3 impossible : normal");
+		}
 
 		System.out.println ( t2 );
 
