161079636883623580f8156e6e9ebb608e246f67
Bruno Mermet <Bruno.Mermet@univ-lehavre.fr>
2017-06-08 14:22:52 +0200

    Création du type Affectation

--- train/circulation/Affectation.java
+++ train/circulation/Affectation.java
@@ -3,6 +3,8 @@ package train.circulation;
 import train.topologie.Itineraire;
 import train.topologie.TypeGare;
 import train.Train;
+import train.vehicule.Wagon;
+import train.vehicule.Voiture;
 
 public class Affectation {
     private Itineraire itineraire;
@@ -10,5 +12,19 @@ public class Affectation {
 
     public Affectation(Itineraire monItineraire, Train monTrain) {
 	TypeGare typeItineraire = monItineraire.getType();
+	Class typeTrain = monTrain.getType();
+	if (compatible(typeItineraire, typeTrain)) {
+	    itineraire = monItineraire;
+	    train = monTrain;
+	}
+	else {
+	    throw new RuntimeException("Incohérence train/trajet");
+	}
+    }
+
+    private boolean compatible(TypeGare typeItineraire, Class typeTrain) {
+	boolean marchandiseCorrect = typeItineraire == TypeGare.MARCHANDISE && typeTrain == Wagon.class;
+	boolean voyageurCorrect = typeItineraire == TypeGare.VOYAGEUR && typeTrain == Voiture.class;
+	return marchandiseCorrect || voyageurCorrect;
     }
 }
